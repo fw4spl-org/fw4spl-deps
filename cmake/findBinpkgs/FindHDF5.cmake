83b983de5497be56804b8e5ef37555032f065b84
Emilie Harquel <emilie.har@gmail.com>
2014-05-15 15:19:03 +0200

    Fixed FindHDF5 (for Release mode)
    --HG--
    branch : fw4spl_0.10.0

--- cmake/findBinpkgs/FindHDF5.cmake
+++ cmake/findBinpkgs/FindHDF5.cmake
@@ -17,7 +17,7 @@ FIND_PATH(HDF5_INCLUDE_DIR cpp/H5Cpp.h
 
 
 FIND_LIBRARY(HDF5_LIBRARY 
-  NAMES hdf5ddll hdf5 hdf5d hdf5_debug
+  NAMES hdf5ddll hdf5dll hdf5 hdf5d hdf5_debug
   HINTS
   $ENV{HDF5_DIR}
   PATH_SUFFIXES lib64 lib
@@ -33,7 +33,7 @@ FIND_LIBRARY(HDF5_LIBRARY
 )
 
 FIND_LIBRARY(HDF5_CPP_LIBRARY 
-  NAMES hdf5_cppddll hdf5_cpp hdf5_cppd hdf5_cpp_debug
+  NAMES hdf5_cppddll hdf5_cppdll hdf5_cpp hdf5_cppd hdf5_cpp_debug
   HINTS
   $ENV{HDF5_DIR}
   PATH_SUFFIXES lib64 lib
