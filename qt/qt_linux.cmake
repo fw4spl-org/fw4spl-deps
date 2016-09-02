e3b6ff315b8072a5878dd28eb059f1c69bb8c66f
Flavien Bridault <fbridault@ircad.fr>
2016-09-02 11:15:33 +0200

    Upgraded Qt to 5.7
    --HG--
    branch : fw4spl_0.11.0

--- qt/qt_linux.cmake
+++ qt/qt_linux.cmake
@@ -28,26 +28,23 @@ set(QT_CONFIGURE_CMD ./configure
     -nomake examples
     -nomake tests
     
-    -no-fontconfig
     -no-dbus
+    -no-fontconfig
     
     -opengl desktop
     -qt-xcb
-    -c++11
+    -c++std c++11
     #We now build qt with gstreamer 1.0 (be sure you have installed libgstreamer-1.0-dev and libgstreamer-plugins-base1.0-dev)
     -gstreamer 1.0
 )
 
 set(INSTALL_ROOT "INSTALL_ROOT=${INSTALL_PREFIX_qt}")
 
-set(QT_PATCH_CMD ${PATCH_EXECUTABLE} -p1 -i ${QT_PATCH_DIR}/MouseEvents.diff -d <SOURCE_DIR>)
-
 ExternalProject_Add(
     qt
     URL ${CACHED_URL}
     DOWNLOAD_DIR ${ARCHIVE_DIR}
-    URL_HASH MD5=${QT5_HASHSUM}
-    PATCH_COMMAND ${QT_PATCH_CMD}
+    URL_HASH SHA256=${QT5_HASHSUM}
     BUILD_IN_SOURCE 1
     DEPENDS zlib jpeg libpng tiff icu4c freetype
     CONFIGURE_COMMAND ${ENV_WRAPPER} ${QT_CONFIGURE_CMD}
