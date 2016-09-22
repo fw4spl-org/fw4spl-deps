7980203e4452f60ba1b49284b5fa91e9d00f4f25
atrog <none@none>
2016-09-22 10:16:29 +0200

    FIXED patch.cmake following issues : . var PRGM_FILES_X86 was wrong - too many ')' . find_program(...) was used with NAME instead of NAMES . find_program(...) given path for 'mysgit-Git-1.9.5-preview20150319/' should have ended with '/bin' . when building for target ANDROID, we must not use  CMAKE_FIND_ROOT_PATH to find_program, which is used by default - so changed that using CMAKE_FIND_ROOT_PATH_MODE_PROGRAM . we did not check properly if we were on _host_ system WIN32 . find path script (find in ProgramFilesX64 and ProgramFilesX86)
    --HG--
    branch : fw4spl_0.11.0

--- cmake/utils/patch.cmake
+++ cmake/utils/patch.cmake
@@ -2,36 +2,78 @@ cmake_minimum_required(VERSION 3.0)
 
 if(NOT PATCH_EXECUTABLE)
 
-    message(STATUS "Checking for patch")
-    set(PRGM_FILES_X86 "ProgramFiles(x86))")
+    message(STATUS "Checking for patch...")
+    
+    
+    # if we are building for Android, we should not use CMAKE_FIND_ROOT_PATH when finding a program
+    # so we disable it temporarily, and we will reset it after find_program
+    if( ANDROID )
+        set( TMP_PREVIOUS_CMAKE_FIND_ROOT_PATH_MODE_PROGRAM ${CMAKE_FIND_ROOT_PATH_MODE_PROGRAM})
+        set( CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER )
+    endif()
+    
+    #necessary hack
+    set(PRGM_FILES_X86 "ProgramFiles(x86)")
+    
     find_program(PATCH_EXECUTABLE 
-        NAME patch
+        NAMES patch patch.exe
         PATHS "usr/bin"
-              "$ENV{ProgramFiles}/GnuWin32/bin"
+              "$ENV{ProgramW6432}/GnuWin32/bin"
+              "$ENV{ProgramW6432}/Git/bin"
+              "$ENV{ProgramW6432}/Git/usr/bin"
               "$ENV{${PRGM_FILES_X86}}/GnuWin32/bin"
-              "$ENV{ProgramFiles}/Git/bin"
               "$ENV{${PRGM_FILES_X86}}/Git/bin"
-              "${CMAKE_BINARY_DIR}/patch/msysgit-Git-1.9.5-preview20150319"
+              "$ENV{${PRGM_FILES_X86}}/Git/usr/bin"
+              "${CMAKE_BINARY_DIR}/patch/msysgit-Git-1.9.5-preview20150319/bin"
     )
     
+    # if we are building for Android, now we have to
+    # reset CMAKE_FIND_ROOT_PATH_MODE_PROGRAM
+    if( ANDROID )
+        set( CMAKE_FIND_ROOT_PATH_MODE_PROGRAM ${TMP_PREVIOUS_CMAKE_FIND_ROOT_PATH_MODE_PROGRAM} )
+    endif()  
+    
+    
     if(NOT PATCH_EXECUTABLE)
-        if(WIN32)
+    
+        message(STATUS "...not found in the given directories.")
+        
+        if(CMAKE_HOST_WIN32 OR WIN32)
+        
+            # make sure the patch does not exist, and that we missed it by error
+            if(EXISTS "${CMAKE_BINARY_DIR}/patch/msysgit-Git-1.9.5-preview20150319/bin/patch.exe") 
+                message(FATAL_ERROR "File ${CMAKE_BINARY_DIR}/patch/msysgit-Git-1.9.5-preview20150319/bin/patch.exe exists but var PATCH_EXECUTABLE was still not set")
+            endif()
+        
             #download git-patch for Windows
+            message(STATUS "Downloading git-patch for Windows")
             set(PATCH_URL https://github.com/msysgit/msysgit/archive/Git-1.9.5-preview20150319.tar.gz)
             file(DOWNLOAD ${PATCH_URL} ${CMAKE_BINARY_DIR}/patch/Git-1.9.5-preview20150319.tar.gz
                  SHOW_PROGRESS
                  EXPECTED_MD5 297d65910bac4097a5b079b1bc36917e)
+                 
+            #uncompressing 
+            message(STATUS "Uncompressing git-patch for Windows")
             execute_process(COMMAND ${CMAKE_COMMAND} -E tar xvf Git-1.9.5-preview20150319.tar.gz
                             WORKING_DIRECTORY "${CMAKE_BINARY_DIR}/patch/")
-            set(PATCH_EXECUTABLE "${CMAKE_BINARY_DIR}/patch/msysgit-Git-1.9.5-preview20150319/bin/patch.exe")
+                            
+            #checking it's really ok
+            if(NOT EXISTS "${CMAKE_BINARY_DIR}/patch/msysgit-Git-1.9.5-preview20150319/bin/patch.exe") 
+                message(FATAL_ERROR "File ${CMAKE_BINARY_DIR}/patch/msysgit-Git-1.9.5-preview20150319/bin/patch.exe was still not found")                
+            endif()
+                     
+            #done !
+            set(PATCH_EXECUTABLE "${CMAKE_BINARY_DIR}/patch/msysgit-Git-1.9.5-preview20150319/bin/patch.exe")                   
+            message(STATUS "Done")
+            
         else()
-            message(FATAL_ERROR "Patch not found")
+            message(FATAL_ERROR "ABORT : Patch not found.")
         endif()
     endif()
     
-    message(STATUS "Found patch: ${PATCH_EXECUTABLE}")
+    message(STATUS "SUCCESS : Found patch ${PATCH_EXECUTABLE}.")
     
-    set(PATCH_EXECUTABLE ${PATCH_EXECUTABLE} CACHE STRING "Patch executable" FORCE )
+    set(PATCH_EXECUTABLE ${PATCH_EXECUTABLE} CACHE FILEPATH "Patch executable" FORCE )
     
 endif()
 
