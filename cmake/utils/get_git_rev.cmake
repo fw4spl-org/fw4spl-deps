68b353e268a3ccd7f70017d6c41106ed40217625
Julien WAECHTER <julien.waechter@gmail.com>
2017-05-11 09:46:16 +0200

    fix(git-tag): updated find tag command
    Now we used tag name if we are exactly on (annotated) tag,
    or <tag>-<n>-g<shortened sha-1> if not, where <n> is number of commits since <tag>

--- cmake/utils/get_git_rev.cmake
+++ cmake/utils/get_git_rev.cmake
@@ -30,9 +30,11 @@ execute_process(
     OUTPUT_STRIP_TRAILING_WHITESPACE
 )
 
-# finds last tag name
+# finds last tag name if you are exactly on (annotated) tag, 
+# or <tag>-<n>-g<shortened sha-1> if not
+# where <n> is number of commits since <tag>
 execute_process(
-    COMMAND git describe --abbrev=0 --tags
+    COMMAND git describe --tags
     WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
     OUTPUT_VARIABLE GIT_TAG
     OUTPUT_STRIP_TRAILING_WHITESPACE
@@ -41,4 +43,4 @@ execute_process(
 # parse GIT_TAG information into pieces.
 string(REGEX REPLACE "^([0-9]+)\\..*" "\\1" GIT_TAG_MAJOR "${GIT_TAG}")
 string(REGEX REPLACE "^[0-9]+\\.([0-9]+).*" "\\1" GIT_TAG_MINOR "${GIT_TAG}")
-string(REGEX REPLACE "^[0-9]+\\.[0-9]+\\.([0-9]+).*" "\\1" GIT_TAG_PATCH "${GIT_TAG}")
+string(REGEX REPLACE "^[0-9]+\\.[0-9]+\\.(.*)" "\\1" GIT_TAG_PATCH "${GIT_TAG}")
